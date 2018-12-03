8a61457c8598dae1580122b87cf3feab524f1689
RomanKarpov <roman.karpov@devexpress.com>
2018-12-03 18:03:21 +0300

    Publish source code.

---
+++ CS/PrintCrosshairSample/Data.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintCrosshairSample {
+    public class SaleItemRepository {
+        readonly static List<String> companies = new List<String> { "DevAV North", "DevAV South", "DevAV West", "DevAV East", "DevAV Central" };
+        readonly static Dictionary<string, List<string>> categorizedProducts = new Dictionary<string, List<string>> {
+            { "Cameras",                new List<string>() { "Camera", "Camcorder", "Binoculars", "Flash", "Tripod" } },
+            { "Cell Phones",            new List<string>() { "Smartphone", "Mobile Phone", "Smart Watch", "Sim Card" } },
+            { "Computers",              new List<string>() { "Desktop", "Laptop", "Tablet", "Printer" } },
+            { "TV, Audio",              new List<string>() { "Television", "Home Audio", "Headphone", "DVD Player" } },
+            { "Vehicle Electronics",    new List<string>() { "GPS Unit", "Radar", "Car Alarm", "Car Accessories" } },
+        };
+
+        public List<SaleItem> GetProductsByMonths() {
+            Random rnd = new Random();
+            List<SaleItem> items = new List<SaleItem>();
+            foreach (string company in companies)
+                foreach (string product in categorizedProducts["Cameras"]) {
+                    DateTime dateTime = new DateTime(2017, 12, 01);
+                    for (int i = 0; i < 12; i++) {
+                        int income = rnd.Next(20, 100);
+                        int revenue = income + rnd.Next(20, 50);
+                        items.Add(new SaleItem() {
+                            Company = company,
+                            Product = product,
+                            Month = dateTime.AddMonths(1).ToString("MMMM", CultureInfo.InvariantCulture),
+                            Income = income,
+                            Revenue = revenue
+                        });
+                        dateTime = dateTime.AddMonths(1);
+                    }
+                }
+            return items;
+        }
+        public List<SaleItem> GetProductsByCompany(int companyIndex) {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            List<SaleItem> items = new List<SaleItem>();
+            foreach (string category in categorizedProducts.Keys) {
+                foreach (string product in categorizedProducts[category]) {
+                    int income = rnd.Next(20, 100);
+                    int revenue = income + rnd.Next(20, 50);
+                    items.Add(new SaleItem() {
+                        Company = companies[companyIndex],
+                        Product = product,
+                        Income = income,
+                        Revenue = revenue,
+                        Category = category
+                    });
+                }
+            }
+            return items;
+        }
+        public List<SaleItem> GetProductsIncome() {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            List<SaleItem> items = new List<SaleItem>();
+            for (int i = 0; i < 50; i++)
+                foreach (string product in categorizedProducts["Cameras"]) {
+                    items.Add(new SaleItem() {
+                        Product = product,
+                        Income = rnd.Next(20, 100)
+                    });
+                }
+            return items;
+        }
+        public List<SaleItem> GetTotalIncome() {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            DateTime now = DateTime.Now;
+            DateTime endDate = new DateTime(now.Year, now.Month, 1);
+            List<SaleItem> items = new List<SaleItem>();
+            foreach (string company in companies) {
+                double companyFactor = rnd.NextDouble() * 0.6 + 1;
+                foreach (string category in categorizedProducts.Keys) {
+                    double categoryFactor = rnd.NextDouble() * 0.6 + 1;
+                    foreach (string product in categorizedProducts[category]) {
+                        int maxIncome = rnd.Next(60, 140);
+                        for (int i = 0; i < 1000; i++) {
+                            if (i % 100 == 0)
+                                maxIncome = Math.Max(40, rnd.Next(maxIncome - 20, maxIncome + 20));
+                            DateTime date = endDate.AddDays(-i - 1);
+                            double income = rnd.Next(20, maxIncome) * companyFactor * categoryFactor;
+                            items.Add(new SaleItem() { Category = category, Company = company, Product = product, OrderDate = date, Income = income });
+                        }
+                    }
+                }
+            }
+            return items;
+        }
+    }
+
+    public class SaleItem {
+        public string Product { get; set; }
+        public string Company { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Month { get; set; }
+        public double Income { get; set; }
+        public double Revenue { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ CS/PrintCrosshairSample/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace PrintCrosshairSample {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.ribbonControl = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.bciPrintCrosshair = new DevExpress.XtraBars.BarCheckItem();
+            this.bbiShowPrintPreview = new DevExpress.XtraBars.BarButtonItem();
+            this.rpChartSettings = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.rpgPrintOptions = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.chartControl = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonStatusBar1
+            // 
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 697);
+            this.ribbonStatusBar.Name = "ribbonStatusBar1";
+            this.ribbonStatusBar.Ribbon = this.ribbonControl;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(1278, 22);
+            // 
+            // ribbonControl
+            // 
+            this.ribbonControl.ExpandCollapseItem.Id = 0;
+            this.ribbonControl.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl.ExpandCollapseItem,
+            this.bciPrintCrosshair,
+            this.bbiShowPrintPreview});
+            this.ribbonControl.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl.MaxItemId = 6;
+            this.ribbonControl.Name = "ribbonControl";
+            this.ribbonControl.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.rpChartSettings});
+            this.ribbonControl.Size = new System.Drawing.Size(1278, 154);
+            this.ribbonControl.StatusBar = this.ribbonStatusBar;
+            // 
+            // bciPrintCrosshair
+            // 
+            this.bciPrintCrosshair.Caption = "Print Crosshair";
+            this.bciPrintCrosshair.Id = 4;
+            this.bciPrintCrosshair.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bciPrintCrosshair.ImageOptions.SvgImage")));
+            this.bciPrintCrosshair.Name = "bciPrintCrosshair";
+            this.bciPrintCrosshair.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.OnPrintCrosshairItemClick);
+            // 
+            // bbiShowPrintPreview
+            // 
+            this.bbiShowPrintPreview.Caption = "Show Print Preview";
+            this.bbiShowPrintPreview.Id = 5;
+            this.bbiShowPrintPreview.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiShowPrintPreview.ImageOptions.SvgImage")));
+            this.bbiShowPrintPreview.Name = "bbiShowPrintPreview";
+            this.bbiShowPrintPreview.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.OnShowPrintPreviewItemClick);
+            // 
+            // rpChartSettings
+            // 
+            this.rpChartSettings.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.rpgPrintOptions});
+            this.rpChartSettings.Name = "rpChartSettings";
+            this.rpChartSettings.Text = "Chart Settings";
+            // 
+            // rpgPrintOptions
+            // 
+            this.rpgPrintOptions.ItemLinks.Add(this.bciPrintCrosshair);
+            this.rpgPrintOptions.ItemLinks.Add(this.bbiShowPrintPreview);
+            this.rpgPrintOptions.Name = "rpgPrintOptions";
+            this.rpgPrintOptions.Text = "Print Options";
+            // 
+            // chartControl
+            // 
+            this.chartControl.CrosshairEnabled = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl.Legend.Name = "Default Legend";
+            this.chartControl.Location = new System.Drawing.Point(0, 154);
+            this.chartControl.Name = "chartControl";
+            this.chartControl.PaletteName = "Office 2013";
+            this.chartControl.SeriesSelectionMode = DevExpress.XtraCharts.SeriesSelectionMode.Point;
+            this.chartControl.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControl.Size = new System.Drawing.Size(1278, 543);
+            this.chartControl.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 719);
+            this.Controls.Add(this.chartControl);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.ribbonControl);
+            this.Name = "MainForm";
+            this.Ribbon = this.ribbonControl;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl;
+        private DevExpress.XtraBars.Ribbon.RibbonPage rpChartSettings;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup rpgPrintOptions;
+        private DevExpress.XtraBars.BarCheckItem bciPrintCrosshair;
+        private DevExpress.XtraBars.BarButtonItem bbiShowPrintPreview;
+        private DevExpress.XtraCharts.ChartControl chartControl;
+    }
+}
+
